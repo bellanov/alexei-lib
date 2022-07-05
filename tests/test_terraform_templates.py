be4425a03e98a2f5c3181c7438b0602fc8917986
Bellanov Apilli <BellanovApilli@gmail.com>
2022-07-05 00:08:54 -0400

    ISSUE-9: Solidify Cloud Build

---
+++ tests/test_terraform_templates.py
@@ -0,0 +1,12 @@
+"""Test Terraform Templates."""
+
+import pytest
+
+from alexei_lib.terraform.templates.client import CLIENT, OUTPUT
+
+
+@pytest.mark.unit
+def test_client_template():
+    """Validate package is importable"""
+    assert CLIENT
+    assert OUTPUT
